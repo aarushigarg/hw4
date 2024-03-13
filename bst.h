70d50c11b9a5a4c003cbce5f5e1981c4efdf2449
Aarushi Garg <aarushigarg@gmail.com>
2024-03-13 11:50:16 -0700

    m

--- bst.h
+++ bst.h
@@ -455,11 +455,6 @@ void BinarySearchTree<Key, Value>::insert(const std::pair<const Key, Value> &key
     Node<Key, Value>* temp = root_;
     Node<Key, Value>* parent = NULL;
 
-    if (root_ == NULL) {
-        root_ = new Node<Key, Value>(keyValuePair.first, keyValuePair.second, NULL);
-        return;
-    }
-
     while (temp != NULL) {
         parent = temp;
         if (keyValuePair.first < temp->getKey()) {
